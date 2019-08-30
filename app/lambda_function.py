c129d4926fbd93c7ca879e9944e810ba89fb0491
Dmitry Poliuha <dm.poliuha@gmail.com>
2019-08-30 21:44:09 +0300

    add alexa responce/request

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -14,10 +14,11 @@ from ask_sdk_model import Response
 
 
 SKILL_NAME = "Google Play Music"
-HELP_MESSAGE = "You can say play John Lennon Imagine"
-HELP_REPROMPT = "What can I help you with?"
+GREETING_MESSAGE = "This is Play Music skill."
+HELP_MESSAGE = "You can say: play John Lennon Imagine"
+HELP_REPROMPT = "What song do you want to hear?"
 STOP_MESSAGE = "Goodbye!"
-FALLBACK_MESSAGE = "Google Play Music skill can't help you with that.  It can help you to listen music from Google Play Music."
+FALLBACK_MESSAGE = "I can't help you with that. I can help you to listen music from Google Play Music."
 FALLBACK_REPROMPT = 'What can I help you with?'
 EXCEPTION_MESSAGE = "Sorry. I cannot help you with that."
 
@@ -26,93 +27,28 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-# Built-in Intent Handlers
-class GetNewFactHandler(AbstractRequestHandler):
-    """Handler for Skill Launch and GetNewFact Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (is_request_type("LaunchRequest")(handler_input) or
-                is_intent_name("GetNewFactIntent")(handler_input))
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
+    handler_input.response_builder.speak(GREETING_MESSAGE).ask(HELP_REPROMPT)
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In GetNewFactHandler")
+    return handler_input.response_builder.response
 
-        song =
-        speech = GET_FACT_MESSAGE + random_fact
 
-        handler_input.response_builder.speak(speech).set_card(
-            SimpleCard(SKILL_NAME, random_fact))
+@sb.request_handler(can_handle_func=is_intent_name("PlayMusicIntent"))
+def play_music_intent(handler_input):
+    slots = handler_input.request_envelope.request.intent.slots
+    song = slots['song'].value
+    if song:
+        logger.info(song)
+        handler_input.response_builder.speak(song)
+        # os.getenv("TG_TOKEN")
+        return handler_input.response_builder.response
+    else:
+        speech = "Please, specify song"
+        handler_input.response_builder.speak(speech)
         return handler_input.response_builder.response
 
 
-class HelpIntentHandler(AbstractRequestHandler):
-    """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.HelpIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In HelpIntentHandler")
-
-        handler_input.response_builder.speak(HELP_MESSAGE).ask(
-            HELP_REPROMPT).set_card(SimpleCard(
-                SKILL_NAME, HELP_MESSAGE))
-        return handler_input.response_builder.response
-
-
-class CancelOrStopIntentHandler(AbstractRequestHandler):
-    """Single handler for Cancel and Stop Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
-                is_intent_name("AMAZON.StopIntent")(handler_input))
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In CancelOrStopIntentHandler")
-
-        handler_input.response_builder.speak(STOP_MESSAGE)
-        return handler_input.response_builder.response
-
-
-class FallbackIntentHandler(AbstractRequestHandler):
-    """Handler for Fallback Intent.
-
-    AMAZON.FallbackIntent is only available in en-US locale.
-    This handler will not be triggered except in that locale,
-    so it is safe to deploy on any locale.
-    """
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In FallbackIntentHandler")
-
-        handler_input.response_builder.speak(FALLBACK_MESSAGE).ask(
-            FALLBACK_REPROMPT)
-        return handler_input.response_builder.response
-
-
-class SessionEndedRequestHandler(AbstractRequestHandler):
-    """Handler for Session End."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("SessionEndedRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        logger.info("In SessionEndedRequestHandler")
-
-        logger.info("Session ended reason: {}".format(
-            handler_input.request_envelope.request.reason))
-        return handler_input.response_builder.response
-
-
-# Exception Handler
 class CatchAllExceptionHandler(AbstractExceptionHandler):
     """Catch all exception handler, log exception and
     respond with custom message.
@@ -126,41 +62,10 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
         logger.info("In CatchAllExceptionHandler")
         logger.error(exception, exc_info=True)
 
-        handler_input.response_builder.speak(EXCEPTION_MESSAGE).ask(
-            HELP_REPROMPT)
+        handler_input.response_builder.speak(EXCEPTION_MESSAGE).ask(HELP_REPROMPT)
 
         return handler_input.response_builder.response
 
 
-# Request and Response loggers
-class RequestLogger(AbstractRequestInterceptor):
-    """Log the alexa requests."""
-    def process(self, handler_input):
-        # type: (HandlerInput) -> None
-        logger.debug("Alexa Request: {}".format(
-            handler_input.request_envelope.request))
-
-
-class ResponseLogger(AbstractResponseInterceptor):
-    """Log the alexa responses."""
-    def process(self, handler_input, response):
-        # type: (HandlerInput, Response) -> None
-        logger.debug("Alexa Response: {}".format(response))
-
-
-# Register intent handlers
-sb.add_request_handler(GetNewFactHandler())
-sb.add_request_handler(HelpIntentHandler())
-sb.add_request_handler(CancelOrStopIntentHandler())
-sb.add_request_handler(FallbackIntentHandler())
-sb.add_request_handler(SessionEndedRequestHandler())
-
-# Register exception handlers
 sb.add_exception_handler(CatchAllExceptionHandler())
-
-# TODO: Uncomment the following lines of code for request, response logs.
-# sb.add_global_request_interceptor(RequestLogger())
-# sb.add_global_response_interceptor(ResponseLogger())
-
-# Handler name that is used on AWS lambda
 lambda_handler = sb.lambda_handler()
